de66b74ba75cb6461af4c95dede866cf9b799326
CHARANTHERAJ <150816701+CHARANTHERAJ@users.noreply.github.com>
2024-07-14 12:33:55 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -11,18 +11,18 @@ class Var(object):
     MULTI_CLIENT = False
     API_ID = int(getenv('API_ID', '23089557'))
     API_HASH = str(getenv('API_HASH', '4a98e047ba5b456892b5043107c0a61f'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', '7179955945:AAHZOSchs4BPAS44p3vWrSkyYOahbv9AEtM'))
-    name = str(getenv('name', 'filetolinkvjbot'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '7063008675:AAG2NCbi5JCdbGHs3SwHrcd903SLO3BzsJo'))
+    name = str(getenv('name', 'Vjfiletolink'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002008101396'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002188210369'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5480645152").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Vijay_malia'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Vijay malia'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
